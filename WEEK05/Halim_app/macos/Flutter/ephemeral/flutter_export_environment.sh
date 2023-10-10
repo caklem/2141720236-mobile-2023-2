a313dfa659c2bb666d35ad9a1b56888a31890f8e
caklem <anadclalulack@gmail.com>
2023-10-10 11:37:02 +0700

    week05

---
+++ WEEK05/Halim_app/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\Akuliah polinema\semester 5\pemrog mobile\Flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Akuliah polinema\semester 5\pemrog mobile\2141720236-mobile-2023\WEEK05\app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
